0d04dfb7911c96786b97a4309eca37c220c644f4
m4d3r1t7-maker <m4d3r1t7@gmail.com>
2025-12-14 16:44:39 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+function webmaster() {
+alert("desarrollado con <3 por Benjamin Insaurralde email: m4d3r1t72@gmail.com");
+}
+
+function oscuro() {
+  const body = document.body;
+  body.style.backgroundColor = body.style.backgroundColor === 'black' ? '#F5A94E' : 'black';
+  body.style.color = body.style.color === 'white' ? 'black' : 'white';
+header.style.backgroundColor = header.style.backgroundColor === 'black' ? '#A1661A' : 'black';
+header.style.color = header.style.color === 'white' ? 'black' : 'white';
+
+footer.style.backgroundColor = footer.style.backgroundColor === 'black' ? '#A1661A' : 'black';
+footer.style.color = footer.style.color === 'white' ? 'black' : 'white';
+}
+function boton() {alert("muchas gracias por valorar nuestra receta vienen mas en camino <3")}
